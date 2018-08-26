b6371bcb3398b46d19639e840591de0949bda04e
weineng <weineng.a@gmail.com>
2018-08-26 17:38:46 +0800

    Fix rubocop lint

--- app/controllers/api/v1/post_controller.rb
+++ app/controllers/api/v1/post_controller.rb
@@ -1,53 +1,51 @@
+# frozen_string_literal: true
+
 class Api::V1::PostController < ApplicationController
+  def index
+    posts = Post.order("created_at DESC")
+    render json: {status: "Success", message: "Show all posts", data: posts}, status: :ok
+  end
 
-	def index
-		posts = Post.order('created_at DESC')
-		render json: { status: 'Success', message: 'Show all posts', data: posts }, status: :ok
-	end
+  def show
+    post = Post.find(params[:id])
+    render json: {status: "Success", message: "Show post", data: post}, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {status: "Error", message: "Post not found", data: post}, status: :unprocessable_entity
+  end
 
-	def show
-		begin
-			post = Post.find(params[:id])
-			render json: {status: 'Success', message: 'Show post', data: post}, status: :ok
-		rescue ActiveRecord::RecordNotFound
-			render json: {status: 'Error', message: 'Post not found', data: post}, status: :unprocessable_entity
-		end
-	end
+  def create
+    post = Post.create(post_params)
+    if post.save
+      render json: {status: "Success", message: "Created post", data: post}, status: :ok
+    else
+      render json: {status: "Error", message: "Failed to create post", data: post.errors}, status: :unprocessable_entity
+    end
+  end
 
-	def create
-		post = Post.create(post_params)
-		if post.save
-			render json: { status: 'Success', message: 'Created post', data: post }, status: :ok
-		else
-			render json: { status: 'Error', message: 'Failed to create post', data: post.errors }, status: :unprocessable_entity
-		end
-	end
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    render json: {status: "Success", message: "Destroyed post", data: post}, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {status: "Error", message: "Can't destroy post as post does not exist", data: post},
+           status: :unprocessable_entity
+  end
 
-	def destroy
-		begin
-			post = Post.find(params[:id])
-			post.destroy
-			render json: { status: 'Success', message: 'Destroyed post', data: post }, status: :ok
-		rescue ActiveRecord::RecordNotFound
-			render json: { status: 'Error', message: "Can't destroy post as post does not exist", data: post }, status: :unprocessable_entity
-		end
-	end
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      render json: {status: "Success", message: "Destroyed post", data: post}, status: :ok
+    else
+      render json: {status: "Error", message: "Unable to update post", data: post}, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: {status: "Error", message: "Can't update post as post does not exist", data: post},
+           status: :unprocessable_entity
+  end
 
-	def update
-		begin
-			post = Post.find(params[:id])
-			if post.update_attributes(post_params)
-				render json: { status: 'Success', message: 'Destroyed post', data: post }, status: :ok
-			else
-				render json: { status: 'Error', message: "Unable to update post", data: post }, status: :unprocessable_entity
-			end
-		rescue ActiveRecord::RecordNotFound
-			render json: { status: 'Error', message: "Can't update post as post does not exist", data: post }, status: :unprocessable_entity
-		end
-	end
+  private
 
-	private
-		def post_params
-			params.permit(:title, :body)
-		end
+  def post_params
+    params.permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  namespace 'api' do
-	  namespace 'v1' do
-		  resources :post
-	  end
+  namespace "api" do
+    namespace "v1" do
+      resources :post
+    end
   end
 end
